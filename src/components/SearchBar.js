78bb1dde34f074a4ad45b1da84a355a4b5c0fe9f
Palmi Moller <palmimoller@me.com>
2020-06-03 12:00:36 -0600

    Added Form Submit handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  state = { term: '' };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    // TODO: Add callback from parent component
+  };
+
   render() {
-    return <div>Search Bar</div>;
+    return (
+      <div className="search-bar ui segement">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+              // onChange={event => this.setState({ term: event.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
